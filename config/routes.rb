d67c8d8c445ad7b5896c49d2af8d5c71f894f66e
Kelli Parris <kelli@hsdesignhouse.com>
2018-10-10 12:49:21 -0500

    created pages, added current copy for all but locations,press,blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  # get 'home-full', to: 'static_pages#home-full'
+  get 'about-us', to: 'static_pages#about-us'
+  get 'contact', to: 'static_pages#contact'
+  get 'franchise-faq', to: 'static_pages#franchise-faq'
+  get 'franchise-process', to: 'static_pages#franchise-process'
+  get 'franchise-inquiries', to: 'static_pages#franchise-inquiries'
+  get 'franchise', to: 'static_pages#franchise'
+  get 'retail', to: 'static_pages#retail'
+  get 'services', to: 'static_pages#services'
 
   get '/fb', to: 'social#fb'
   get '/insta', to: 'social#insta'
