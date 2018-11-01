ef2c95c00f64c939ce87ee49f9839f0f5f86de79
Kelli Parris <kelli@hsdesignhouse.com>
2018-11-01 10:55:24 -0500

    finished basic franchise pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,32 @@
 class StaticPagesController < ApplicationController
+	def about_us
+	end
+
+	def contact
+	end
+
+	def doggie_daycare
+	end
+
+	def grooming
+	end
+
+	def home
+	end
+
+	def press
+	end
+
+	def retail
+	end
+
+	def self_service_grooming
+	end
+
+	def services
+	end
+
+	def vet_clinic
+	end
+
 end
