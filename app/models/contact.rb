5c1ad8d030d9c0ba32e71c9c40fdbef9254c9f5d
Kelli Parris <kelli@hsdesignhouse.com>
2018-11-02 09:56:49 -0500

    contact form layout, added live chat

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ApplicationRecord
   validates :email, presence: true
   validates :phone, presence: true
   # validates :location, presence: true
-  validates :singup, presence: true
+  # validates :singup, presence: true
   # validates :grapevine, presence: true
   validates :content, presence: true
 end
