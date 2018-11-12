be97302e490d1d5255d6e84a84a1a03b13254aeb
Kelli Parris <kelli@hsdesignhouse.com>
2018-11-12 16:00:33 -0600

    changes to gmaps

--- app/assets/javascripts/gmap-index.js
+++ app/assets/javascripts/gmap-index.js
@@ -11,13 +11,15 @@ document.addEventListener("turbolinks:load", function() {
 
   locations.forEach(function(location) {
     if (location.latitude && location.longitude) {
+      // var iconBase = '/assets/';
       var marker = map.addMarker({
+        // icon: iconBase + 'marker-pink.png',
         lat: location.latitude,
         lng: location.longitude,
         title: location.address,
         infoWindow: {
-          content: '<p>' + location.street_address_one + '<br>' + location.city + ', ' + location.state + ' ' + location.zip + '<br>' + '<a href="tel:' + location.phone + '">' + location.phone +
-          '</a></p> <p><a href="https://www.google.com/maps/dir//' +
+          content: '<p>' + location.street_address_one + '<br>' + location.city + ', ' + location.state + ' ' + location.zip + '</p><p>' + '<a href="tel:' + location.phone + '">' + location.phone +
+          '</a></p> <p class="email"><a href="mailto:' + location.email_address + '">' + location.email_address + '</a></p> <p><a class="woof-link" href="https://www.google.com/maps/dir//' +
           location.street_address_one + '+' + location.city + '+' + location.state + '+' + location.zip + '" target="_blank">Get Directions</a></p>'
         }
       });
