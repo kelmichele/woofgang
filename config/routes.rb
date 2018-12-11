39a079afa628a3ebfe47636671002aa9be1e6592
Kelli Parris <kelli@hsdesignhouse.com>
2018-12-11 10:20:58 -0600

    mobile header and nav, changed all fonts to helvetica and hid weight 300

--- config/routes.rb
+++ config/routes.rb
@@ -12,33 +12,18 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get 'page', to: 'static_pages#page'
-  get 'home-alt', to: 'static_pages#home-alt'
-  get 'layout', to: 'static_pages#layout'
 
-  get 'about-us', to: 'static_pages#about-us'
-  get 'contact-us', to: 'static_pages#contact-us'
   get 'services', to: 'static_pages#services'
-  get 'press', to: 'static_pages#press'
   get 'grooming', to: 'static_pages#grooming'
   get 'self-service-grooming', to: 'static_pages#self-service-grooming'
   get 'doggie-daycare', to: 'static_pages#doggie-daycare'
   get 'vet-clinic', to: 'static_pages#vet-clinic'
-
-  get 'pet-stores', to: 'static_pages#pet-stores'
   get 'retail', to: 'static_pages#retail'
 
-  get 'franchise-process', to: 'franchise_pages#franchise-process'
   get 'franchising', to: 'franchise_pages#franchising'
-  get 'franchise-opportunity', to: 'franchise_pages#franchise-opportunity'
   get 'training-support', to: 'franchise_pages#training-support'
-  get 'franchisees', to: 'franchise_pages#franchisees'
-  get 'investment-details', to: 'franchise_pages#investment-details'
   get 'real-estate', to: 'franchise_pages#real-estate'
-
   get 'franchise-steps', to: 'franchise_pages#franchise-steps'
-  get 'info', to: 'franchise_pages#info'
-
   get 'faq', to: 'franchise_pages#faq'
 
   get '/fb', to: 'social#fb'
