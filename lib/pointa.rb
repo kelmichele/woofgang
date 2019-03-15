a487d3af225e771477167945aa4ae6638d7b5609
kelmichele <kelli@hsdesignhouse.com>
2019-03-15 14:21:34 -0500

    switch ipap back. locations down

--- lib/pointa.rb
+++ lib/pointa.rb
@@ -3,8 +3,8 @@ module Pointa
   require 'json'
 
   def get_pointa ip_address
-    # pointa = Net::HTTP.get(URI("https://ipapi.co/#{ip_address}/json/"))
-    pointa = ("https://api.ipapi.com/api/check?access_key=" + Rails.application.credentials.ipapi[:ipapi_access])
+    pointa = Net::HTTP.get(URI("https://ipapi.co/#{ip_address}/json/"))
+    # pointa = ("https://api.ipapi.com/api/check?access_key=" + Rails.application.credentials.ipapi[:ipapi_access])
 
     JSON.parse(pointa)
   end
