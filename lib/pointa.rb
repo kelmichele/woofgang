a7ec23bfa66d92cbcc22391dd5a703c2bf0df0f5
kelmichele <kelli@hsdesignhouse.com>
2019-03-18 09:22:41 -0500

    switch back

--- lib/pointa.rb
+++ lib/pointa.rb
@@ -3,9 +3,9 @@ module Pointa
   require 'json'
 
   def get_pointa ip_address
-    # pointa = Net::HTTP.get(URI("https://ipapi.co/#{ip_address}/json/"))
-    pointa = Net::HTTP.get(URI("ipinfo.io"))
-    # pointa = ("https://api.ipapi.com/api/check?access_key=" + Rails.application.credentials.ipapi[:ipapi_access])
+    pointa = Net::HTTP.get(URI("https://ipapi.co/#{ip_address}/json/"))
+    # pointa = Net::HTTP.get(URI("ipinfo.io"))
+    #alt? pointa = ("https://api.ipapi.com/api/check?access_key=" + Rails.application.credentials.ipapi[:ipapi_access])
 
     JSON.parse(pointa)
   end
