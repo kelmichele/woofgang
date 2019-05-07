9869995ebd2be3534142fc8ceb3c51247a642927
kelmichele <kelli@hsdesignhouse.com>
2019-05-07 09:34:08 -0500

    Added fonts in VENDORS folder

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Woofgang
     config.load_defaults 5.2
 		config.autoload_paths += %W(#{config.root}/lib)
 
+		config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+		config.assets.precompile << /\.(?:eot|woff|ttf)$/
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
