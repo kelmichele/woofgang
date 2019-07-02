d60191d856776b7d1153d744d649fab8394d3e1c
kelmichele <kelli@hsdesignhouse.com>
2019-07-02 09:00:55 -0500

    added s3 to shrine

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,13 +1,29 @@
 require 'shrine'
-require 'shrine/storage/file_system'
 
-Shrine.storages = {
-  # temporary storage
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
+if Rails.env.development?
+	require 'shrine/storage/file_system'
+	Shrine.storages = {
+		# temporary storage
+	  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
 
-  # permanent storage
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store'),
-}
+	  # permanent storage
+	  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store')
+	}
+else
+  require "shrine/storage/s3"
+
+	s3_options = {
+	  access_key_id:     Rails.application.credentials.aws[:access_key_id],
+	  secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+	  region:            "us-east-1",
+	  bucket:            "woofgang"
+	}
+
+	Shrine.storages = {
+	  cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+	  store: Shrine::Storage::S3.new(prefix: "store", **s3_options)
+	}
+end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for forms
