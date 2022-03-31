77fc3405fb721296cd37c22eea17a38115225ad1
kelmichele <kelli@hsdesignhouse.com>
2022-03-31 12:17:11 -0500

    last state redirect tweak

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,5 +1,6 @@
 class StatesController < ApplicationController
-	before_action :set_state, only: [ :edit, :update, :destroy, :show]
+	# before_action :set_state, only: [ :edit, :update, :destroy, :show]
+	before_action :set_state, only: [ :edit, :update, :destroy]
 	before_action :authenticate_user!, except: [:index, :show]
   before_action :require_admin, except: [:index, :show]
 
